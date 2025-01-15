bf5983f424e28449f29c5e182eb61ff1c9ac4179
webOps-aman <amanhxe@gmail.com>
2025-01-15 16:39:02 +0530

    add product in admin panel done

---
+++ e-commerce-1/admin/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Admin from "../src/Components/Pages/Admin/Admin";
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Admin/>
+    </>
+  );
+}
+
+export default App;
