6e5f5d8b8b535b5fd731375c8660fda3791ba586
webOps-aman <amanhxe@gmail.com>
2025-01-22 20:10:29 +0530

    completed hero section, latest collection

--- shoppingApp/frontend/src/components/Navbar.jsx
+++ shoppingApp/frontend/src/components/Navbar.jsx
@@ -10,7 +10,9 @@ const Navbar = () => {
   return (
    <>
     <div className='flex items-center justify-between py-5 font-medium'>
-        <img src={assets.logo} alt="" className='w-36'/>
+        <Link to='/'>
+            <img src={assets.logo} alt="" className='w-36'/>
+        </Link>
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
             <NavLink to="/" className="flex flex-col items-center gap-1">
                 <p>HOME</p>
